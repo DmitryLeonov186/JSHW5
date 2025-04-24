87d1b518a2bc18c21a0b99bd8918befaeb8937bf
Dmitry Leonov <lemomonster@mail.ru>
2025-04-24 22:51:38 +0500

    Task 2

--- features/search.feature
+++ features/search.feature
@@ -1,5 +1,23 @@
-Feature: Search a course
-    Scenario: Should search by text
-        Given user is on "/navigation" page
-        When user search by "тестировщик"
-        Then user sees the course suggested "Тестировщик ПО"
+Feature: Ticket booking
+    Scenario: One ticket
+        Given this user is visited the page "/client/index.php"
+        When the user is choose the day of the booking week "4"
+        When the user is choose the show time and movie title "225"
+        When the user has chose any free seat in the movie hall
+        When the user is click on to the «Reservation» button
+        Then sees the booking confirmation with the name of the movie "Ведьмак"
+    Scenario: Two tickets
+        Given this user is visited the page "/client/index.php"
+        When the user is choose the day of the booking week "5"
+        When the user is choose the show time and movie title "217"
+        When the user has chose any free seat in the movie hall
+        When the user has chose any free seat in the movie hall
+        When the user is click on to the «Reservation» button
+        Then sees the booking confirmation with the name of the movie "Сталкер(1979)"
+    Scenario: Sad case
+        Given this user visited the page "/client/index.php"
+        When the user is choose the day of the booking week "5"
+        When the user is choose the show time and movie title "199"
+        When the user is choose a seat in the hall that is not available for booking
+        When the user is try to click on to the «Reservation» button
+        Then the user is understands that the «Reservation» button is inactive
--- features/step_definitions/search.steps.js
+++ features/step_definitions/search.steps.js
@@ -2,7 +2,8 @@ const puppeteer = require("puppeteer");
 const chai = require("chai");
 const expect = chai.expect;
 const { Given, When, Then, Before, After } = require("cucumber");
-const { putText, getText } = require("../../lib/commands.js");
+const { clickElement, putText, getText } = require("../../lib/commands.js");
+const { timeout } = require("puppeteer");
 
 Before(async function () {
   const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
@@ -17,18 +18,87 @@ After(async function () {
   }
 });
 
-Given("user is on {string} page", async function (string) {
-  return await this.page.goto(`https://netology.ru${string}`, {
-    setTimeout: 20000,
-  });
+Given("this user is visited the page {string}", async function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return await this.page.goto(`http://qamid.tmweb.ru${string}`);
 });
 
-When("user search by {string}", async function (string) {
-  return await putText(this.page, "input", string);
+When(
+  "the user is choose the day of the booking week {string}",
+  async function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return await clickElement(this.page, `a:nth-child(${string})`);
+  }
+);
+
+When(
+  "the user is choose the show time and movie title {string}",
+  async function (sessionId) {
+    // Write code here that turns the phrase above into concrete actions
+    return await clickElement(
+      this.page,
+      `a[href='#'][data-seance-id='${sessionId}']`
+    );
+  }
+);
+
+When("the user has chose any free seat in the movie hall", async function () {
+  // Write code here that turns the phrase above into concrete actions
+  return await clickElement(
+    this.page,
+    `span[class='buying-scheme__chair buying-scheme__chair_standart']`
+  );
 });
 
-Then("user sees the course suggested {string}", async function (string) {
-  const actual = await getText(this.page, "a[data-name]");
-  const expected = await string;
-  expect(actual).contains(expected);
+When("the user is click on to the «Reservation» button", async function () {
+  // Write code here that turns the phrase above into concrete actions
+  return await this.page.click(`.acceptin-button`);
 });
+
+Then(
+  "sees the booking confirmation with the name of the movie {string}",
+  async function (string) {
+    const actual = await getText(this.page, `.ticket__details.ticket__title`);
+    const expected = await string;
+    // Write code here that turns the phrase above into concrete actions
+    return expect(actual).contains(expected);
+  }
+);
+
+Given("this user visited the page {string}", async function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return await this.page.goto(`http://qamid.tmweb.ru${string}`);
+});
+
+When(
+  "the user is choose a seat in the hall that is not available for booking",
+  async function () {
+    // Write code here that turns the phrase above into concrete actions
+    return await clickElement(
+      this.page,
+      `span[class='buying-scheme__chair buying-scheme__chair_taken']`
+    );
+  }
+);
+
+When(
+  "the user is try to click on to the «Reservation» button",
+  async function () {
+    // Write code here that turns the phrase above into concrete actions
+    return await clickElement(
+      this.page,
+      "span[class='buying-scheme__chair buying-scheme__chair_taken']"
+    );
+  }
+);
+
+Then(
+  "the user is understands that the «Reservation» button is inactive",
+  async function () {
+    expected = true;
+    const acceptinButton = await this.page.$(".acceptin-button");
+    const actual = await acceptinButton.evaluate((btn) => btn.disabled);
+    // Write code here that turns the phrase above into concrete actions
+    return expect(actual).equal(expected);
+  }
+);
